da90a01516daf4d104ad8d0462f15cdf1d835b5e
Jonathan Fei <52461351+jfei3339@users.noreply.github.com>
2021-12-20 17:46:05 -0500

    save as UTF
    save as UTF

--- src/me/Jon/SurvivalGames/SQL/MySQL.java
+++ src/me/Jon/SurvivalGames/SQL/MySQL.java
@@ -17,7 +17,7 @@ public class MySQL {
 	private String host = "localhost";
 	private String port = "3306";
 	private String database = "playerdata";
-	private String username = "";
+	private String username = "root";
 	private String password = "";
 	
 	private Connection connection;
